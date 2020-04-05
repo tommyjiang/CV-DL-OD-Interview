b03b1435ea0bf23a3b9b82e9814fa87d79e16c6b
Tommy-iMac <tommy_jiang@foxmail.com>
2020-04-05 17:38:18 +0800

    2020-04-05 17:40

--- coding/OJ/Kick Start/2020/Round A/P4. Bundling/bundling.py
+++ coding/OJ/Kick Start/2020/Round A/P4. Bundling/bundling.py
@@ -14,15 +14,48 @@ if __name__ == '__main__':
     """
 
     """
+    class Trie:
+        def __init__(self):
+            """
+            Initialize your data structure here.
+            """
+            self.root = dict()
+
+        def insert(self, word):
+
+            cur = self.root
+            for c in word:
+                if not c in cur:
+                    cur[c] = [dict(), 1]
+                else:
+                    cur[c] = [cur[c][0], cur[c][1] + 1]
+                cur = cur[c][0]
+
     set_debug(True)
     t = int(input())
     for ti in range(1, t + 1):
-        n, k = [int(s) for s in input().split()]
-
+        n, K = [int(s) for s in input().split()]
         strings = []
+
         for _ in range(n):
             strings.append(input())
 
-        print(strings)
+        t = Trie()
 
-        print("Case #{}: {}".format(ti, 0))
+        for s in strings:
+            t.insert(s)
+
+        q = collections.deque()
+        q.append(t.root)
+
+        res = 0
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+
+                for k in cur:
+                    res += cur[k][1] // K
+                    q.append(cur[k][0])
+
+        print(f'Case #{ti}: {res}')
+
