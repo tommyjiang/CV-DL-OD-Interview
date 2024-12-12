008947011493374357bfde45b9e9624c22808247
Hao Jiang <tommy_jiang@foxmail.com>
2024-12-12 12:02:46 +0800

    update quick sort code

--- coding/Algorithm/Sort/quick_sort.py
+++ coding/Algorithm/Sort/quick_sort.py
@@ -1,28 +1,31 @@
 import random
+swap = 0
+
 def quick_sort(A, l, r):
     if l < r:
         q = partition(A, l, r)
-        # print(A, A[q])
         quick_sort(A, l, q - 1)
         quick_sort(A, q + 1, r)
 
 
 def partition(A, l, r):
+    global swap
     s = random.randint(l, r)
     A[s], A[r] = A[r], A[s]
 
     x = A[r]
-    i = l - 1
+    i = l
     for j in range(l, r):
         if A[j] <= x:
-            i += 1
             A[i], A[j] = A[j], A[i]
-    A[i + 1], A[r] = A[r], A[i+1]
+            swap += 1
+            i += 1
+    A[i], A[r] = A[r], A[i]
 
-    return i + 1
+    return i
 
 
-A = [8, 3, 5, 1, 0, 6, 9, 2, 7, 4]
+A = [8, 3, 5, 9, 6, 1, 2, 7, 4]
 quick_sort(A, 0, len(A)-1)
 print(A)
-
+print(swap)
