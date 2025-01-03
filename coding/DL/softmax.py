eca05adff051a473b7bb748edd3fe15848636a4f
Hao Jiang <tommy_jiang@foxmail.com>
2025-01-03 08:52:02 +0800

    update softmax

--- coding/DL/softmax.py
+++ coding/DL/softmax.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def softmax(x):
-    y = np.exp(x - x.max())
-    return y / y.sum()
+    y = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return y / np.sum(y, axis=1, keepdims=True)
 
-x = np.array([1, 2, 3])
+x = np.array([[1, 2, 3], [2, 4, 6]])
 print(softmax(x))
